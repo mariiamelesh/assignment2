afbe2624a99ef4dd722c0f4333eaa84a42d3816c
skibidi <mariia.melesh.25@kse.org.ua>
2026-03-22 17:23:07 +0200

    Created BreadthFirstSearch.cs

---
+++ BreadthFirstSearch.cs
@@ -0,0 +1,10 @@
+namespace Kse.Algorithms.Assignment2
+{
+    public class BreadthFirstSearch
+    {
+        public (List<Point>, int) FindPath(string[,] map, Point start, Point end)
+        {
+          
+		}
+	}
+}
