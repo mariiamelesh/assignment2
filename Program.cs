1f9410ec19454265c3bfb47930b4c7c7e1ea6b16
skibidi <mariia.melesh.25@kse.org.ua>
2026-03-22 17:01:57 +0200

    Set Seed to 1; added fake point list(path) to check modified Print() method

--- Program.cs
+++ Program.cs
@@ -5,10 +5,17 @@ namespace Kse.Algorithms.Assignment2 {
 			{
 				Height = 10,
 				Width = 100,
+				Seed = 1,
 			};
+			
+			var point = new Point(0, 0);
+			var point1 = new Point(38, 4);
+			var point2 = new Point(98, 8);
+			var list = new List<Point> {point, point1, point2};
+			
 			var generator = new MapGenerator(optionsToGenerate);
 			string[,]? map = generator.Generate();
-			new MapPrinter().Print(map);
+			new MapPrinter().Print(map, list);
 		}
 	}
 }
